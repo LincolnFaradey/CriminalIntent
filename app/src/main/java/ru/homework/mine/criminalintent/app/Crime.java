4aab9420af010784a038d5aaa82dd3dd059c98bd
LincolnFaradey <iphoenix179@gmail.com>
2014-03-09 22:28:07 -0400

    First Steps

---
+++ app/src/main/java/ru/homework/mine/criminalintent/app/Crime.java
@@ -0,0 +1,49 @@
+package ru.homework.mine.criminalintent.app;
+
+import android.text.format.DateFormat;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by faradey on 3/9/14.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
