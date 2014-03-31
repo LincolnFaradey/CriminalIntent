758c5eaae3b6eea647ffbe4bf68d12899eda4611
LincolnFaradey <iphoenix179@gmail.com>
2014-03-30 20:52:29 -0400

    Date Picker dialog

--- app/src/main/java/ru/homework/mine/criminalintent/app/DatePickerFragment.java
+++ app/src/main/java/ru/homework/mine/criminalintent/app/DatePickerFragment.java
@@ -5,8 +5,11 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by faradey on 3/30/14.
@@ -31,9 +34,26 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         View v = getActivity().getLayoutInflater()
                 .inflate(R.layout.dialog_date, null);
+
+        DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mDate = new GregorianCalendar(year, month, day).getTime();
+
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
