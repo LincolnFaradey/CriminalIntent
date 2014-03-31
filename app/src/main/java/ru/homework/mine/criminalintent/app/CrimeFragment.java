d864801376b41bfe57a75d6e1b851a5c297eef1f
LincolnFaradey <iphoenix179@gmail.com>
2014-03-30 20:30:56 -0400

    Date Picker dialog

--- app/src/main/java/ru/homework/mine/criminalintent/app/CrimeFragment.java
+++ app/src/main/java/ru/homework/mine/criminalintent/app/CrimeFragment.java
@@ -2,6 +2,7 @@ package ru.homework.mine.criminalintent.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -28,6 +29,7 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
 
     public static final String EXTRA_CRIME_ID = "ru.homework.mine.criminalIntent.app.CRIME_ID";
+    private static final String DIALOG_DATE = "date";
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -72,7 +74,15 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button) v.findViewById(R.id.crime_data);
         mDateButton.setText(DateFormat.format("EEEE, MMM dd, yyyy", mCrime.getDate()));
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getActivity()
+                        .getSupportFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(fm, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
