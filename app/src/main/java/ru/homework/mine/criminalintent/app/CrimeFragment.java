1765baec0e5382e41be45dff304bbfb2e1d2b5e4
LincolnFaradey <iphoenix179@gmail.com>
2014-03-30 20:44:33 -0400

    Date Picker dialog

--- app/src/main/java/ru/homework/mine/criminalintent/app/CrimeFragment.java
+++ app/src/main/java/ru/homework/mine/criminalintent/app/CrimeFragment.java
@@ -79,7 +79,7 @@ public class CrimeFragment extends Fragment {
             public void onClick(View v) {
                 FragmentManager fm = getActivity()
                         .getSupportFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(fm, DIALOG_DATE);
             }
         });
--- app/src/main/java/ru/homework/mine/criminalintent/app/DatePickerFragment.java
+++ app/src/main/java/ru/homework/mine/criminalintent/app/DatePickerFragment.java
@@ -6,10 +6,28 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 
+import java.util.Date;
+
 /**
  * Created by faradey on 3/30/14.
  */
 public class DatePickerFragment extends DialogFragment {
+
+    public static final String EXTRA_DATE =
+            "ru.homehell.mine.criminalIntent.date";
+
+    private Date mDate;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
